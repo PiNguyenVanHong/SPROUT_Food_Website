5cc82641b8394023cbca581207b3e56c0fa7477b
PiKayQi <pi.nguyenvanhong@gmail.com>
2024-11-04 06:40:59 +0700

    06: Edit Readme and show some picture

--- client/src/routes/404-page.tsx
+++ client/src/routes/404-page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import NotFoundImg from "@/assets/404.png";
 
 function NotFoundPage() {
     return (
         <div className="w-full h-full flex flex-col items-center gap-10 py-20">
             <h1 className="font-semibold text-3xl">Opps, this page couldn't be found</h1>
             <div className="w-1/2">
-                <img src="@/assets/404.png" alt="404 Image" />
+                <img src={NotFoundImg} alt="404 Image" />
             </div>
             <div className="flex items-center gap-5">
                 <Button className="bg-foreground-red text-background hover:opacity-80 hover:bg-foreground-red transition duration-300" size={"lg"}>
